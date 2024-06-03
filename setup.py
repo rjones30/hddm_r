ed9933318b8556d97783da04c1e15642c04b57c5
Richard Jones <rjones30@gmail.com>
2024-06-03 12:36:25 -0400

    * more work on HDDM builds with streaming_input, bump version [rtj]

--- setup.py
+++ setup.py
@@ -178,7 +178,7 @@ else:
                           ]
 setuptools.setup(
     name = "hddm_r",
-    version = "2.0.5",
+    version = "2.0.6",
     url = "https://github.com/rjones30/hddm_r",
     author = "Richard T. Jones",
     description = "i/o module for GlueX reconstructed events",
