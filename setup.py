4840758ecb0c50f5d67956fbbf9db0255fb617b6
Richard Jones <rjones30@gmail.com>
2024-06-03 18:41:10 -0400

    * add static library libXrdUtils_static.a to the link list [rtj]

--- setup.py
+++ setup.py
@@ -189,10 +189,11 @@ else:
                            "crypto",
                            "xrootdstream",
                            "XrdCl_static",
+                           "XrdUtils_static",
                           ]
 setuptools.setup(
     name = "hddm_r",
-    version = "2.0.10",
+    version = "2.0.11",
     url = "https://github.com/rjones30/hddm_r",
     author = "Richard T. Jones",
     description = "i/o module for GlueX reconstructed events",
