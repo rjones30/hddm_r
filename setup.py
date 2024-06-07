0e8e36661750034848b9de00e61edab9ad1269ef
Richard Jones <rjones30@gmail.com>
2024-06-06 20:27:11 -0400

    * bump the version number again, cannot see changes from latest release [rtj]

--- setup.py
+++ setup.py
@@ -199,7 +199,7 @@ else:
                           ]
 setuptools.setup(
     name = "hddm_r",
-    version = "2.0.14",
+    version = "2.0.15",
     url = "https://github.com/rjones30/hddm_r",
     author = "Richard T. Jones",
     description = "i/o module for GlueX reconstructed events",
