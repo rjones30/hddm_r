263e5d65863b372d72c52e5bc60aefa1074c0439
Richard Jones <rjones30@gmail.com>
2024-06-24 13:23:20 -0400

    * apply fixes from HDDM [rtj]

--- setup.py
+++ setup.py
@@ -233,7 +233,7 @@ if "macos" in sysconfig.get_platform():
 
 setuptools.setup(
     name = "hddm_r",
-    version = "2.0.20",
+    version = "2.0.21",
     url = "https://github.com/rjones30/hddm_r",
     author = "Richard T. Jones",
     description = "i/o module for GlueX reconstructed events",
