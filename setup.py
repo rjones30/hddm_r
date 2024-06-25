895ffaea2ec9102bf9c3bd7360dd68fbd59a0fc7
Richard Jones <rjones30@gmail.com>
2024-06-25 15:37:12 -0400

    * remove the dummy __init__.py that interfere with the proper   loading of the extension library modules [rtj]

--- setup.py
+++ setup.py
@@ -234,7 +234,7 @@ if "macos" in sysconfig.get_platform():
 
 setuptools.setup(
     name = "gluex.hddm_r",
-    version = "1.0.7",
+    version = "1.0.8",
     url = "https://github.com/rjones30/hddm_r",
     author = "Richard T. Jones",
     description = "i/o module for GlueX reconstructed events",
